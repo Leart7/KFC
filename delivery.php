667452b082beeeb4f367a21353f22eb422891da7
Leart Shabani <leartshabani77@gmail.com>
2023-04-28 20:32:39 +0200

    modals added

--- delivery.php
+++ delivery.php
@@ -116,3 +116,188 @@ if($session->isSignedIn()){
     ?>
     </div>
 </div>
+
+<?php
+          $products = $product->find_all();
+          foreach($products as $produkti){
+            $id = $produkti->getId();
+            echo "<div class='modal' id='myModal$id'> ";
+            echo " <div class='modal-dialog modal-dialog-scrollable modal-xl' id='modalwidth'>";
+            echo " <div class='modal-content'>";
+            echo "<div class='modal-header'>";
+            echo "<h4 class='modal-title ms-4 text-danger'>{$produkti->getEmri_produktit()}</h4>";
+            echo "<button type='button' class='btn-close' data-bs-dismiss='modal'></button>";
+            echo "</div>";
+            echo "<div class='modal-body'>";
+            echo "<p class='ms-4 text-dark' style='font-size: 18px;'>Photo Gallery</p>";
+            echo "<div class='ms-4' style='height:15vh'>";
+            echo "<img src='images/{$produkti->getPhoto()}' alt='' class='h-75'>";
+            echo "</div>";
+            echo "<p class='ms-4 text-dark' style='font-size: 18px;''>Shto Menu</p>";
+            echo "<div class='form-check ms-5'>";
+            echo "<input class='form-check-input bg-secondary shadow-none' type='checkbox' id='check1' name='option1' value='something' >";
+            echo "<label class='form-check-label  w-100' for='check1'>Menu me Potato Wedges + Pije</label>";
+            echo "</div>";
+            echo "<p class='ms-4 text-dark mt-4' style='font-size: 18px;'>SHTO (max 10)</p>";
+            echo "<div class='form-check ms-5'>";
+            echo "<div class='my-3 d-flex formdiv'>";
+            echo "<input class='form-check-input bg-secondary shadow-none' type='checkbox' id='check2' name='option1' value='something' >";
+            echo "<label class='form-check-label w-100 ps-2' for='check2'>Potato Wedges</label>";
+            echo "<p class='text-danger me-3'>+1.49€</p>";
+            echo " </div>";
+            echo "<div class='my-3 d-flex formdiv'>";
+            echo "<input class='form-check-input bg-secondary shadow-none' type='checkbox' id='check2' name='option1' value='something' >";
+            echo "<label class='form-check-label w-100 ps-2' for='check2'>Mayonaisse</label>";
+            echo "<p class='text-danger me-3'>+0.10€</p>";
+            echo " </div>";
+            echo "<div class='my-3 d-flex formdiv'>";
+            echo "<input class='form-check-input bg-secondary shadow-none' type='checkbox' id='check2' name='option1' value='something' >";
+            echo "<label class='form-check-label w-100 ps-2' for='check2'>Ketchup</label>";
+            echo "<p class='text-danger me-3'>+0.10€</p>";
+            echo " </div>";
+            echo "<div class='my-3 d-flex formdiv'>";
+            echo "<input class='form-check-input bg-secondary shadow-none' type='checkbox' id='check2' name='option1' value='something' >";
+            echo "<label class='form-check-label w-100 ps-2' for='check2'>Heineken</label>";
+            echo "<p class='text-danger me-3'>+2.49€</p>";
+            echo " </div>";
+            echo "<div class='my-3 d-flex formdiv'>";
+            echo "<input class='form-check-input bg-secondary shadow-none' type='checkbox' id='check2' name='option1' value='something' >";
+            echo "<label class='form-check-label w-100 ps-2' for='check2'>Uje</label>";
+            echo "<p class='text-danger me-3'>+0.79€</p>";
+            echo " </div>";
+            echo "<div class='my-3 d-flex formdiv'>";
+            echo "<input class='form-check-input bg-secondary shadow-none' type='checkbox' id='check2' name='option1' value='something' >";
+            echo "<label class='form-check-label w-100 ps-2' for='check2'>Miser</label>";
+            echo "<p class='text-danger me-3'>+0.69€</p>";
+            echo " </div>";
+            echo "</div>";
+            echo "<p class='ms-4 text-dark' style='font-size: 18px;'>Special Instructions</p>";
+            echo "<form class='mx-4'>";
+            echo "<div class='mb-3'>";
+            echo " <input type='text' class='form-control shadow-none pb-5'  id='exampleInputEmail1' aria-describedby='emailHelp' placeholder='Add any special instructions' style='height: 8vh;''>";
+            echo "</div>";
+            echo "</form>";
+            echo "</div>";
+            echo "<div class='modal-footer d-flex align-items-center justify-content-between'>";
+            echo "<form method='post' target='frame'>";
+            echo "<button class='btn btn-transparent shadow-none text-secondary ms-4 minus' style='font-size: 50px;' name='minus{$produkti->getId()}'><p class='minuscontent'>-</p></button>";
+            echo "<button class='btn btn-transparent shadow-none text-danger me-auto plus' style='font-size: 50px;' name='plus{$produkti->getId()}'><p>+</p></button>";
+            echo "</form>";
+            
+              
+            
+            echo "<form method='post'>";
+            echo "<div class='d-flex'>";
+            
+            echo "<button class='btn btn-danger px-5 py-3 d-flex justify-content-center align-items-center fw-bold butoni ' name='shto{$produkti->getId()}'><input class='px-2 py-2 number' name='vlera{$produkti->getId()}' value='1'>Add</button>";
+            echo "</div>";
+            echo "</form>";
+            // echo "<input class='m-4' class='cmimi' value='{$produkti['cmimi']}'>";
+            echo "</div>";
+            echo "</div>";
+            echo "</div>";
+            echo "</div>";
+            $numri = 0;
+            if(isset($_POST['shto'.$produkti->getId()])){
+              $sasia = $_POST['vlera'.$produkti->getId()];
+              $numri = $sasia;
+              $cart->insertCart($produkti->getId(),$user->getId(), $numri);
+              echo("<script>location.href = 'delivery.php'</script>");
+          }
+
+      }      
+            $value = $cart->getSasiaTotale($user->getId());
+            echo "<input type='hidden' id='vlerasasise' class='fw-bold display-1 text-dark' value='{$value}'>";
+        ?>
+
+<?php
+          $products = $product->find_all();
+          foreach($products as $produkti){
+            $id = $produkti->getId();
+            $sasia = $cart->getSasiaProduktit($id,$user->getId());
+            
+            echo "<div class='modal' id='myModaledit$id'>";
+            echo " <div class='modal-dialog modal-dialog-scrollable modal-xl'>";
+            echo " <div class='modal-content'>";
+            echo "<div class='modal-header'>";
+            echo "<h4 class='modal-title ms-4 text-danger'>{$produkti->getEmri_produktit()}</h4>";
+            echo "<button type='button' class='btn-close' data-bs-dismiss='modal'></button>";
+            echo "</div>";
+            echo "<div class='modal-body'>";
+            echo "<p class='ms-4 text-dark' style='font-size: 18px;'>Photo Gallery</p>";
+            echo "<div class='ms-4' style='height:15vh'>";
+            echo "<img src='images/{$produkti->getPhoto()}' alt='' class='h-75'>";
+            echo "</div>";
+            echo "<p class='ms-4 text-dark' style='font-size: 18px;''>Shto Menu</p>";
+            echo "<div class='form-check ms-5'>";
+            echo "<input class='form-check-input bg-secondary shadow-none' type='checkbox' id='check1' name='option1' value='something' >";
+            echo "<label class='form-check-label  w-100' for='check1'>Menu me Potato Wedges + Pije</label>";
+            echo "</div>";
+            echo "<p class='ms-4 text-dark mt-4' style='font-size: 18px;'>SHTO (max 10)</p>";
+            echo "<div class='form-check ms-5'>";
+            echo "<div class='my-3 d-flex formdiv'>";
+            echo "<input class='form-check-input bg-secondary shadow-none' type='checkbox' id='check2' name='option1' value='something' >";
+            echo "<label class='form-check-label w-100 ps-2' for='check2'>Potato Wedges</label>";
+            echo "<p class='text-danger me-3'>+1.49€</p>";
+            echo " </div>";
+            echo "<div class='my-3 d-flex formdiv'>";
+            echo "<input class='form-check-input bg-secondary shadow-none' type='checkbox' id='check2' name='option1' value='something' >";
+            echo "<label class='form-check-label w-100 ps-2' for='check2'>Mayonaisse</label>";
+            echo "<p class='text-danger me-3'>+0.10€</p>";
+            echo " </div>";
+            echo "<div class='my-3 d-flex formdiv'>";
+            echo "<input class='form-check-input bg-secondary shadow-none' type='checkbox' id='check2' name='option1' value='something' >";
+            echo "<label class='form-check-label w-100 ps-2' for='check2'>Ketchup</label>";
+            echo "<p class='text-danger me-3'>+0.10€</p>";
+            echo " </div>";
+            echo "<div class='my-3 d-flex formdiv'>";
+            echo "<input class='form-check-input bg-secondary shadow-none' type='checkbox' id='check2' name='option1' value='something' >";
+            echo "<label class='form-check-label w-100 ps-2' for='check2'>Heineken</label>";
+            echo "<p class='text-danger me-3'>+2.49€</p>";
+            echo " </div>";
+            echo "<div class='my-3 d-flex formdiv'>";
+            echo "<input class='form-check-input bg-secondary shadow-none' type='checkbox' id='check2' name='option1' value='something' >";
+            echo "<label class='form-check-label w-100 ps-2' for='check2'>Uje</label>";
+            echo "<p class='text-danger me-3'>+0.79€</p>";
+            echo " </div>";
+            echo "<div class='my-3 d-flex formdiv'>";
+            echo "<input class='form-check-input bg-secondary shadow-none' type='checkbox' id='check2' name='option1' value='something' >";
+            echo "<label class='form-check-label w-100 ps-2' for='check2'>Miser</label>";
+            echo "<p class='text-danger me-3'>+0.69€</p>";
+            echo " </div>";
+            echo "</div>";
+            echo "<p class='ms-4 text-dark' style='font-size: 18px;'>Special Instructions</p>";
+            echo "<form class='mx-4'>";
+            echo "<div class='mb-3'>";
+            echo " <input type='text' class='form-control shadow-none pb-5'  id='exampleInputEmail1' aria-describedby='emailHelp' placeholder='Add any special instructions' style='height: 8vh;''>";
+            echo "</div>";
+            echo "</form>";
+            echo "</div>";
+            echo "<div class='modal-footer d-flex align-items-center justify-content-between'>";
+            echo "<form method='post' target='frame'>";
+            echo "<button class='btn btn-transparent shadow-none text-secondary ms-4 minustwo' style='font-size: 50px;' name='minustwo{$produkti->getId()}'><p class='minuscontenttwo'>-</p></button>";
+            echo "<button class='btn btn-transparent shadow-none text-danger me-auto plustwo' style='font-size: 50px;' name='plustwo{$produkti->getId()}'><p>+</p></button>";
+            echo "</form>";
+            
+              
+            
+            echo "<form method='post'>";
+            echo "<div class='d-flex'>";
+            echo "<button class='btn btn-danger px-5 py-3 d-flex justify-content-center align-items-center fw-bold butoni' style='width:500px' name='shtotwo{$produkti->getId()}'><input class='px-3 py-2 numbertwo ' name='vleratwo{$produkti->getId()}' value='{$sasia}'>OK</button>";
+            echo "</div>";
+            echo "</form>";
+            echo "</div>";
+            echo "</div>";
+            echo "</div>";
+            echo "</div>";
+            $numri = 0;
+            if(isset($_POST['shtotwo'.$produkti->getId()])){
+              $sasia = $_POST['vleratwo'.$produkti->getId()];
+              $numri = $sasia;
+              $cart->updateCart($produkti->getId(), $numri, $user->getId());
+              echo("<script>location.href = 'delivery.php'</script>");
+              }
+
+}
+
+        ?>
