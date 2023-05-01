7efd70fbd3bbda362293ddcab70ac7fab0e2859e
Leart Shabani <leartshabani77@gmail.com>
2023-05-01 21:41:01 +0200

    admin header&sidebar

---
+++ AdminDashboard/sidebar.php
@@ -0,0 +1,23 @@
+<div class="deznav">
+            <div class="deznav-scroll">
+				<ul class="metismenu" id="menu">
+                    <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+							<i class="flaticon-dashboard-1"></i>
+							<span class="nav-text">Dashboard</span>
+						</a>
+                        <ul aria-expanded="false">
+							<li><a href="order-list.php">Orders</a></li>
+							<li><a href="customer-list.php">Customers</a></li>
+							<li><a href="ecom-product-grid.php">Products</a></li>
+									<li><a href="categories.php">Categories</a></li>
+						</ul>
+
+                    </li>
+      
+                </ul>
+            
+				<div class="plus-box">
+					<a class="btn bg-white text-black btn-rounded d-block" href="addproduct.php">+Add Menus</a>
+				</div>
+			</div>
+        </div>
